99f30bd9129a0aa4603b5963287ff0b7940f8130
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2017-12-27 03:02:39 -0800

    Node Homework

--- keys.js
+++ keys.js
@@ -1,9 +1,15 @@
+console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: 'dAhRhjLp0UOvNQ9eQDWJp03z3',
   consumer_secret: 'wJg1UKU4qQav2i6JOXeCZVI7a4i4AaBujeUpDe8q5mhgiatj9S',
-  access_token_key: 'wJg1UKU4qQav2i6JOXeCZVI7a4i4AaBujeUpDe8q5mhgiatj9S',
-  access_token_secret: 'O0FK5fhlt4G7z25AqLcP4QPFZGPYDMUdEi74eGBct86J4',
+  access_token_key: "943379294050443264-FnSrm6gREuNpSOPOkZOYqLdS6Qbbxk0",
+  access_token_secret: "O0FK5fhlt4G7z25AqLcP4QPFZGPYDMUdEi74eGBct86J4",
 }
 
+
+
 module.exports = twitterKeys;
+
+
+
